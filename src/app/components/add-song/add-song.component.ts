d3239876b0d8d930b15bbe4f252b43941b8e6aaa
ashin <ashin.mahat@extensodata.com>
2020-08-31 23:02:28 +0545

    inital

--- src/app/components/add-song/add-song.component.ts
+++ src/app/components/add-song/add-song.component.ts
@@ -22,10 +22,14 @@ export class AddSongComponent implements OnInit {
 
     this.meta.addTags([
       {name: 'description', content: 'This is swornim bung industry .It is Swonim bung industry located in kathmandu nepal.Sworinim pipe industry is Smoking pipe manufacturar.'},
-      {name: 'author', content: 'Swornim mahat,saru mahat,ashin mahat'},
+      {name: 'author', content: "Swornim pipe industry nepal,Swornim Factory Nepal,Swornim Pipe Industry,"+
+      "Swornim Mahat Industry Nepal kathmandu,Bung Industry in nepal,weed pipe factory nepal,Smoking pipe factory nepal,"+
+     "Weed pipe industry kathmandu nepal,Swornim pipe industry nepal,nepal craft shop,nepal smoking pipe,nepal handicraft,handicraft nepal"},
       {name: 'keywords', content: "Swornim pipe industry nepal,Swornim Factory Nepal,Swornim Pipe Industry,"+
                            "Swornim Mahat Industry Nepal kathmandu,Bung Industry in nepal,weed pipe factory nepal,Smoking pipe factory nepal,"+
-                          "Weed pipe industry kathmandu nepal,Swornim pipe industry nepal"}
+                          "Weed pipe industry kathmandu nepal,Swornim pipe industry nepal,nepal craft shop,nepal smoking pipe,nepal handicraft,handicraft nepal"},
+       {name:'google-site-verification', content:"nJmpxDdin1_k1GKfwTWkbUgarLMCb_zAAczgi0mKHjg"}
+
     ]);
     this.setTitle('Swornim pipe industry nepal.Swornim smoking pipe industry kathmandu');
   }
