2451be6a74dba85e6cd57ce9cf2c0de0d866c9f3
ashin <ashin.mahat@extensodata.com>
2020-08-31 21:39:31 +0545

    tag

--- src/app/components/songs/songs.component.ts
+++ src/app/components/songs/songs.component.ts
@@ -18,11 +18,13 @@ export class SongsComponent implements OnInit {
     });
 
     this.meta.addTags([
-      {name: 'description', content: 'Swornim bung industry'},
-      {name: 'author', content: 'Swornim bung industy'},
-      {name: 'keywords', content: 'Sworniim,Bung,Industry,Swornim Factory,Saru Pipe Industry,Swornim pipe industry,Swornim Mahat Industry'}
+      {name: 'description', content: 'This is swornim bung industry .It is Swonim bung industry located in kathmandu nepal.Sworinim pipe industry is Smoking pipe manufacturar.'},
+      {name: 'author', content: 'Swornim mahat,saru mahat,ashin mahat'},
+      {name: 'keywords', content: "Swornim pipe industry nepal,Swornim Factory Nepal,Swornim Pipe Industry,"+
+                           "Swornim Mahat Industry Nepal kathmandu,Bung Industry in nepal,weed pipe factory nepal,Smoking pipe factory nepal,"+
+                          "Weed pipe industry kathmandu nepal,Swornim pipe industry nepal"}
     ]);
-    this.setTitle('Home Page');
+    this.setTitle('Swornim pipe industry nepal.Swornim smoking pipe industry kathmandu');
   }
 
   ngOnInit() { }
